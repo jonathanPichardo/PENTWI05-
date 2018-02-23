eab58cfd4bbc2f7c5626b31545ad4e93e7fab992
Jonathan Israel Pichardo Ortega <jonathan.pichardo@softtek.com>
2018-02-23 14:08:31 -0800

    Validation

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
-(function () {
+(function() {
 
 
 
-    const nameRegex = /[\sa-z]*/i;
+    const nameRegex = /[\sa-z]+/i;
     const emailRegex = /\w*/i;
 
+    document.getElementById('submit-button').onclick = submitForm;
 
-    function validateField(fieldId, regexp) {
-
+    function handleErrors(error) {
+        console.error(error);
+        alert('An unexpected error has ocurred');
     }
 
-    function submitForm() {
+    function validateFieldWithRegexp(fieldId, regexp) {
+        return regexp.test(document.getElementById(fieldId).value)
+    }
 
+    function validateForm(inputs) {
+        return inputs.reduce((test, input) => test && validateFieldWithRegexp(input.id, input.pattern), true);
+    }
+
+    function submitForm($event) {
+
+        try {
+
+            const inputs = [
+                { id: 'studentNumberField', pattern: nameRegex },
+                { id: 'firstNameField', pattern: nameRegex },
+                { id: 'lastNameField', pattern: nameRegex },
+                { id: 'emailField', pattern: emailRegex }
+            ];
+
+            validateForm(inputs) ? alert('Form Correctly Submitted') : alert('Incorrect Form');
+        } catch (e) {
+            handleErrors(e);
+        }
     }
 
 
